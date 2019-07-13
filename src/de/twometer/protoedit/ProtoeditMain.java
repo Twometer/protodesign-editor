69cf8fb5cf30ecc5d7c3e02b188bc2912cea552e
Twometer <twometer@outlook.de>
2019-07-13 18:57:20 +0200

    Add basic JavaFX app

--- src/de/twometer/protoedit/ProtoeditMain.java
+++ src/de/twometer/protoedit/ProtoeditMain.java
@@ -1,9 +1,24 @@
 package de.twometer.protoedit;
 
-public class ProtoeditMain {
+import de.twometer.protoedit.util.ResourceLoader;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class ProtoeditMain extends Application {
 
     public static void main(String[] args) {
+        launch(args);
+    }
 
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(ResourceLoader.getResource("layout/main.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
 }
